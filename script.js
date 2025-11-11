8230b495904bd5d426e954cb6e8fa18846f8918f
shreecom <harshadaspatil1999@gmail.com>
2025-11-11 07:44:24 -0800

    latest_version

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ slider.addEventListener('mouseleave', () => {
 });
 
 // Smooth scroll for navigation links
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+document.querySelectorAll('.slides').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
         const target = document.querySelector(this.getAttribute('href'));
